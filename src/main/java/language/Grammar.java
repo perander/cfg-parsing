3bd35dcc42ebedee4bc7bdc6c63598d6a12e4afa
perander <silva.perander@helsinki.fi>
2019-11-19 23:25:28 +0100

    update Grammar, new tests passed

--- src/main/java/language/Grammar.java
+++ src/main/java/language/Grammar.java
@@ -9,15 +9,22 @@ public class Grammar {
     private List<Rule> rules; //could be a set
 
     //one parent to many children of possibly many elements (S -> NP VP | VP NP)
-    private HashMap<String, List<List<String>>> parentToChild = new HashMap<>();
+    private HashMap<String, List<List<String>>> parentToChild;
 
     //possibly many parents to one children of possibly many elements (S -> NP VP, NP -> NP VP)
-    private HashMap<List<String>, List<String>> childToParent = new HashMap<>();
+    private HashMap<List<String>, List<String>> childToParent;
 
     public Grammar() {
         this.rules = new ArrayList<>();
+        this.parentToChild = new HashMap<>();
+        this.childToParent = new HashMap<>();
     }
 
+    /**
+     * Adds a new rule to the grammar and updates the child-parent-mappings.
+     *
+     * @param rule
+     */
     public void addRule(Rule rule) {
         this.rules.add(rule);
         this.updateMapping(rule);
@@ -63,6 +70,79 @@ public class Grammar {
         return this.childToParent.get(child);
     }
 
+    /**
+     * Get all rules which have {@code parent} as parent.
+     *
+     * @param parent
+     * @return list of rules
+     */
+    public List<Rule> getRulesByParent(String parent) {
+        List<Rule> selected = new ArrayList<>();
+
+        for (Rule rule : rules) {
+            if (rule.getParent().equals(parent)) {
+                selected.add(rule);
+            }
+        }
+        return selected;
+    }
+
+    /**
+     * Find the starting symbol of the grammar. The symbol does not have a parent and it's not an element of any child.
+     *
+     * @return starting symbol if found, null otherwise
+     */
+    public String getRoot() {
+        for (String parent : getAllParents()) {
+            List<String> parentAsChild = new ArrayList<>();
+            parentAsChild.add(parent);
+
+            if (this.getParentsByChild(parentAsChild) == null && !partOfChild(parent)) {
+                return parent;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks whether a given element is a part of a child of some rule in the grammar.
+     *
+     * @param element
+     * @return true if the element is a part of a child, false otherwise.
+     */
+    private boolean partOfChild(String element) {
+        for (List<String> child : getAllChildren()) {
+            if (child.contains(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Finds the terminal symbols of the grammar. The terminal symbols are one-element children with no children themselves.
+     *
+     * @return a list of terminal symbols
+     */
+    public List<String> getTerminals() {
+        List<String> terminals = new ArrayList<>();
+
+        for (List<String> child : getAllChildren()) {
+            if (child.size() == 1) {
+                if (getChildrenByParent(child.get(0)) == null) {
+                    terminals.add(child.get(0));
+                }
+            }
+        }
+
+        return terminals;
+    }
+
+    /**
+     * Updates the parent-child-mappings.
+     *
+     * @param rule
+     */
     private void updateMapping(Rule rule) {
         /*Collecting children to a parent. There might be many, like N -> fish, N -> robots.
         The order of the elements of a child matters, that's why using a list.*/
--- src/test/java/language/GrammarTest.java
+++ src/test/java/language/GrammarTest.java
@@ -16,11 +16,10 @@ import static org.junit.Assert.assertTrue;
 @RunWith(MockitoJUnitRunner.class)
 public class GrammarTest {
     private Grammar grammarEmpty;
-    //testattava
+
     @InjectMocks
     Grammar grammar;
 
-    //testiä varten erikseen tehty
     @Mock
     Rule ruleMock;
 
@@ -28,23 +27,11 @@ public class GrammarTest {
     Rule ruleMock2;
 
     @Before
-    public void setupRule() {
+    public void setup() {
         grammarEmpty = new Grammar();
+        grammar = new Grammar();
 
         ruleMock = Mockito.mock(Rule.class);
-
-        List<String> child = new ArrayList<>();
-        child.add("np");
-        child.add("vp");
-
-        Mockito.when(ruleMock.getParent()).thenReturn("s");
-        Mockito.when(ruleMock.getChild()).thenReturn(child);
-    }
-
-    @Test
-    public void addRuleAddsRule() {
-        grammarEmpty.addRule(ruleMock);
-        List<Rule> rules = grammarEmpty.getRules();
 
         String parent = "s";
 
@@ -52,8 +39,65 @@ public class GrammarTest {
         child.add("np");
         child.add("vp");
 
-        assertTrue(parent.equals(rules.get(0).getParent()));
-        assertTrue(child.equals(rules.get(0).getChild()));
+        Mockito.when(ruleMock.getParent()).thenReturn(parent);
+        Mockito.when(ruleMock.getChild()).thenReturn(child);
+
+        ruleMock2 = Mockito.mock(Rule.class);
+
+        String parent2 = "np";
+
+        List<String> child2 = new ArrayList<>();
+        child2.add("n");
+
+        Mockito.when(ruleMock2.getParent()).thenReturn(parent2);
+        Mockito.when(ruleMock2.getChild()).thenReturn(child2);
+    }
+
+    @Test
+    public void addRuleAddsRule() {
+        grammar.addRule(ruleMock);
+        List<Rule> rules = grammar.getRules();
+
+        String parent = "s";
+
+        List<String> child = new ArrayList<>();
+        child.add("np");
+        child.add("vp");
+
+        assertTrue(1 == grammar.getRules().size());
+
+        Rule rule = rules.get(0);
+
+        assertTrue(ruleMock.equals(rule));
+
+        assertTrue(parent.equals(rule.getParent()));
+        assertTrue(child.equals(rule.getChild()));
+    }
+
+    @Test
+    public void getParentsByChildReturnsCorrectParents() {
+        grammar.addRule(ruleMock);
+        grammar.addRule(ruleMock2);
+
+        String parent = "s";
+
+        List<String> child = new ArrayList<>();
+        child.add("np");
+        child.add("vp");
+
+        assertTrue(grammar.getParentsByChild(child).size() == 1);
+        assertTrue(grammar.getParentsByChild(child).contains(parent));
+    }
+
+    @Test
+    public void getParentsByChildReturnsNullWhenNoParents() {
+        grammar.addRule(ruleMock);
+        grammar.addRule(ruleMock2);
+
+        List<String> child = new ArrayList<>();
+        child.add("s");
+
+        assertTrue(grammar.getParentsByChild(child) == null);
     }
 
     @Test
@@ -62,7 +106,42 @@ public class GrammarTest {
 
         Mockito.verify(ruleMock, Mockito.times(3)).getParent();
         Mockito.verify(ruleMock, Mockito.times(3)).getChild();
+    }
 
+    @Test
+    public void getRootReturnsStartingSymbol() {
+        String root = "s";
+
+        grammar.addRule(ruleMock);
+        grammar.addRule(ruleMock2);
+
+        assertTrue(root.equals(grammar.getRoot()));
+    }
+
+    @Test
+    public void getParentsReturnsAllParents() {
+        grammar.addRule(ruleMock);
+        grammar.addRule(ruleMock2);
+
+        String parent = "s";
+        String parent2 = "np";
+
+        assertTrue(grammar.getAllParents().size() == 2);
+        assertTrue(grammar.getAllParents().contains(parent));
+        assertTrue(grammar.getAllParents().contains(parent2));
+    }
+
+    @Test
+    public void getTerminalsReturnsTerminals() {
+        List<String> terminals = new ArrayList<>();
+        terminals.add("n");
+        terminals.add("vp");
+
+        grammar.addRule(ruleMock);
+        grammar.addRule(ruleMock2);
+
+        assertTrue(grammar.getTerminals().contains("n"));
+        assertTrue(!grammar.getTerminals().contains("np"));
     }
 
 }
