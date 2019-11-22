3bcb0ec993e78acdc99c78b78ab5166a2a35eece
perander <silva.perander@helsinki.fi>
2019-11-22 23:59:22 +0100

    start different kinds of userinterface in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String... args) throws IOException {
 
         UserInterface ui = new UserInterface();
-        ui.start();
+        ui.startUserInputReader();
     }
 }
