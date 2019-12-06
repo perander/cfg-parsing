6c35bcd8cb904b6671ca5249503c3bf35013b7bf
perander <silva.perander@helsinki.fi>
2019-12-06 23:09:50 +0200

    validate inputs, test validator

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -1,10 +1,76 @@
 package utils;
 
 import basicdatastructures.List;
+import language.Grammar;
 import language.Rule;
 
 public class Validator {
 
+    /**
+     * Checks whether the given grammar is in the Chomsky normal form.
+     *
+     * This requires that each rule has 1 or 2 children: either two non-terminal children or one terminal child.
+     * There should not be rules with no children.
+     *
+     * @param grammar
+     * @return true if the grammar is in the Chomsky normal form, false otherwise.
+     */
+    public boolean validateChomsky(Grammar grammar) {
+        List<String> parents = grammar.getAllParents();
+        List<String> terminals = grammar.getTerminals();
+
+        List<Rule> rules = grammar.getRules();
+
+        for (int i = 0; i < rules.size(); i++) {
+            Rule rule = rules.get(i);
+
+            List<String> child = rule.getChild();
+
+            //rule too long
+            if (child.size() > 2){
+                System.out.println("too long: " + child);
+                return false;
+            }
+
+            //mixing terminals and non-terminals
+            if (child.size() > 1) {
+                if ((terminals.contains(child.get(0)) && parents.contains(child.get(1))) ||
+                        (parents.contains(child.get(0)) && terminals.contains(child.get(1)))) {
+                    System.out.println("mixing terminals and non-terminals: " + child);
+                    return false;
+                }
+            }
+
+            //more than one terminal in a row
+            if (terminals.contains(child.get(0)) && child.size() != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks whether the child has too many elements (over 2)
+     *
+     * @param rule
+     * @return true if the child has 2 or fewer elements, false otherwise
+     */
+    public boolean validateRule(Rule rule) {
+        rule.getChild().get(0).split(" ");
+
+        //System.out.println("rule " + rule.getChild() + " size: " + rule.getChild().size());
+        if (rule.getChild() == null || rule.getChild().size() > 2) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Parses the given string to a rule.
+     *
+     * @param s
+     * @return a rule parsed from the string
+     */
     public Rule prepareRule(String s) {
         Rule rule = new Rule();
 
@@ -20,6 +86,57 @@ public class Validator {
 
         rule.setParent(parent);
         rule.setChild(child);
+
+        return rule;
+    }
+
+    /**
+     * In the format S -> NV | VN
+     *
+     * @param s
+     * @return
+     */
+    public Grammar prepareGrammar(String s) {
+        Grammar grammar = new Grammar();
+        //separate rules
+        String[] rules = s.split(":");
+        for (int i = 0; i < rules.length; i++) {
+            //separate parent and children
+            String[] rule = rules[i].split("->");
+            String parent = rule[0];
+            //separate individual children
+            String[] children = rule[1].split("\\|");
+
+            for (String child : children) {
+                grammar.addRule(prepareNewRule(parent.trim(), child.trim()));
+            }
+        }
+        return grammar;
+    }
+
+    /**
+     * In the format S -> NV
+     *
+     * @param parent
+     * @param child
+     * @return
+     */
+    public Rule prepareNewRule(String parent, String child){
+        Rule rule = new Rule();
+        List<String> elements = new List();
+        String[] childElements = child.split(" ");
+
+        for (int i = 0; i < childElements.length; i++) {
+            elements.add(childElements[i]);
+        }
+
+        rule.setParent(parent);
+        rule.setChild(elements);
+
+
+        if (validateRule(rule)) {
+            return rule;
+        }
 
         return rule;
     }
---
+++ src/test/java/utils/ValidatorTest.java
@@ -0,0 +1,96 @@
+package utils;
+
+import basicdatastructures.List;
+import language.Grammar;
+import language.Rule;
+import org.junit.Before;
+import org.junit.Test;
+import ui.UserInterface;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ValidatorTest {
+    private Grammar grammar1, grammar2, grammar3;
+    private String ruleAsString1, ruleAsString2, ruleAsString3;
+
+    private UserInterface ui;
+    private Validator validator;
+
+    @Before
+    public void setup() {
+        ui = new UserInterface();
+        validator = new Validator();
+
+        //incorrect input: rule too long
+        ruleAsString1 = "s -> np vp:" +
+                "vp -> v:" +
+                "np -> n:" +
+                "n -> fish:" +
+                "np -> fish s fish";
+
+
+        //incorrect input: mixing terminals and non-terminals
+        ruleAsString2 = "s -> np vp:" +
+                "vp -> v:" +
+                "np -> n:" +
+                "n -> fish:" +
+                "np -> fish s";
+
+        //correct input
+        ruleAsString3 = "s -> np vp:" +
+                "vp -> v np | vp adv | v:" +
+                "np -> n:" +
+                "v -> fish:" +
+                "n -> fish | robots:" +
+                "adv -> today";
+
+        grammar1 = validator.prepareGrammar(ruleAsString1);
+        grammar2 = validator.prepareGrammar(ruleAsString2);
+        grammar3 = validator.prepareGrammar(ruleAsString3);
+    }
+
+    @Test
+    public void prepareGrammarWorks() {
+        for (int i = 0; i < grammar3.getRules().size(); i++) {
+            System.out.println(grammar3.getRules().get(i));
+        }
+        assertTrue(grammar3.getRules().size() == 9);
+        assertTrue(grammar3.getAllParents().size() == 6);
+    }
+
+    @Test
+    public void validateRuleWorksWithCorrectRules() {
+        List<Rule> rules = grammar3.getRules();
+
+        for (int i = 0; i < rules.size(); i++) {
+            assertTrue(validator.validateRule(rules.get(i)));
+        }
+    }
+
+    @Test
+    public void validateRuleDoesNotWorkWithIncorrectRules() {
+        List<Rule> rules = grammar1.getRules();
+        boolean works = true;
+
+        for (int i = 0; i < rules.size(); i++) {
+            if(!validator.validateRule(rules.get(i))) {
+                works = false;
+            }
+        }
+        assertFalse(works);
+    }
+
+    @Test
+    public void validateChomskyWorksWithCorrectInput() {
+        assertTrue(validator.validateChomsky(grammar3));
+    }
+
+    @Test
+    public void validateChomskyWorksWithIncorrectInput() {
+        System.out.println(grammar2.getTerminals());
+        System.out.println(grammar2.getTerminals().size());
+
+        assertFalse(validator.validateChomsky(grammar2));
+    }
+}
