cc1252880b4d3c89f16ff97a1ae3b1366b4d7c1e
perander <silva.perander@helsinki.fi>
2019-12-20 22:26:03 +0200

    read phrase from file

--- src/main/java/efficiency/EarleyEfficiency.java
+++ src/main/java/efficiency/EarleyEfficiency.java
@@ -3,27 +3,34 @@ package efficiency;
 import language.Grammar;
 import parser.Earley;
 import parser.Parser;
-import ui.UserInterface;
 import utils.Validator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class EarleyEfficiency {
-    public static void main(String... args) {
-        UserInterface ui = new UserInterface();
+    public static void main(String... args) throws IOException {
         Validator validator = new Validator();
 
-        String grammarUnparsed = "s -> np vp:vp -> v np | vp adv | v:np -> n:v -> fish:n -> fish | robots:adv -> today";
-        String phrase = "robots fish fish today";
+        String grammarUnparsed = "s -> l r|l a|p p:p -> l r|l a|p p:a -> p r:l -> (:r -> ):";
+
+        File phraseFile = new File("src/resources/parentheses/phrase.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(phraseFile));
+        String phraseUnparsed = reader.readLine();
 
         Grammar grammar = validator.prepareGrammar(grammarUnparsed);
-        String[] words = validator.preparePhrase(phrase);
+        String[] phrase = validator.preparePhrase(phraseUnparsed);
 
         Parser earley = new Earley(grammar);
 
         long start = System.nanoTime();
-        boolean belongs = earley.belongsToLanguage(grammar, words);
+        boolean belongs = earley.belongsToLanguage(grammar, phrase);
         long end = System.nanoTime();
 
         System.out.println("The phrase belongs to the language: " + belongs);
+        System.out.println("Took " + ((end - start) / 1e6) + "ms");
         System.out.println("Took " + ((end - start) / 1e9) + " s");
     }
 }
